337145d35b1e60c4da7e91c70e723c0eb208abad
aalto95 <stanportableuse@gmail.com>
2025-01-16 16:22:42 +0900

    Add minimal UI for main page GH-2

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-  import { Button } from "$lib/components/ui/button";
+  import Header from "$lib/components/custom/Header.svelte";
+  import MainInput from "$lib/components/custom/MainInput.svelte";
   import { ModeWatcher } from "mode-watcher";
 </script>
 
-<main class="dark:bg-background bg-indigo-50 w-screen h-dvh overflow-x-hidden">
-  <Button>Hello</Button>
+<main class="dark:bg-gray-900 bg-indigo-50 w-screen h-dvh overflow-x-hidden">
+  <Header />
+  <div class="w-full h-dvh pt-14 flex flex-col justify-center items-center">
+    <MainInput />
+  </div>
+
   <ModeWatcher />
 </main>
---
+++ src/lib/components/custom/MainInput.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Input from "../ui/input/input.svelte";
+</script>
+
+<div class="w-full px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20">
+  <h1 class="text-3xl lg:text-4xl xl:text-5xl text-center mb-8">
+    Make your query
+  </h1>
+  <Input
+    class="h-16 xl:h-20 text-2xl xl:text-3xl"
+    on:keypress={(e) => {
+      if (e.code === "Enter") {
+        console.log("enter");
+      }
+    }}
+  />
+</div>
