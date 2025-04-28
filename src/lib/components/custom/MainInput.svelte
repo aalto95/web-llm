c30a0ffd45c7b62bbcadce48239a4fd633084155
aalto95 <stanportableuse@gmail.com>
2025-04-28 14:08:48 +0900

    Create llm inference

--- src/lib/components/custom/MainInput.svelte
+++ src/lib/components/custom/MainInput.svelte
@@ -1,18 +1,87 @@
-<script>
+<script lang="ts">
+  import type {
+    ChatCompletionSystemMessageParam,
+    ChatCompletionUserMessageParam,
+  } from "@mlc-ai/web-llm";
+  import Button from "../ui/button/button.svelte";
   import Input from "../ui/input/input.svelte";
+  import * as webllm from "@mlc-ai/web-llm";
+
+  let engine: webllm.MLCEngine;
+  let reply: webllm.ChatCompletion & AsyncIterable<webllm.ChatCompletionChunk>;
+  let query: string = "";
+  let isQuerying = false;
+
+  async function initLLM() {
+    // Callback function to update model loading progress
+    const initProgressCallback = (initProgress: any) => {
+      console.log(initProgress);
+    };
+    const selectedModel = "Qwen2-0.5B-Instruct-q0f32-MLC";
+
+    engine = await webllm.CreateMLCEngine(
+      selectedModel,
+      { initProgressCallback: initProgressCallback } // engineConfig
+    );
+
+    const messages: (
+      | ChatCompletionSystemMessageParam
+      | ChatCompletionUserMessageParam
+    )[] = [
+      { role: "system", content: "You are a helpful AI assistant." },
+      { role: "user", content: "Hello!" },
+    ];
+
+    reply = await engine.chat.completions.create({
+      messages,
+    });
+
+    console.log(reply.choices[0].message);
+    console.log(reply.usage);
+  }
+
+  async function makeQuery() {
+    console.log(query);
+    isQuerying = true;
+    reply = await engine.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful AI assistant." },
+        { role: "user", content: query },
+      ],
+    });
+    query = "";
+    isQuerying = false;
+
+    console.log(reply.choices[0].message);
+    console.log(reply.usage);
+  }
+
+  initLLM();
 </script>
 
-<div class="w-full">
+<div class="w-full flex flex-col h-full mt-32">
   <h1 class="text-2xl lg:text-3xl xl:text-4xl text-center mb-8">
     Make your query
   </h1>
-  <Input
-    class="w-full h-12 text-lg xl:text-xl"
-    placeholder="Ask away..."
-    on:keypress={(e) => {
-      if (e.code === "Enter") {
-        console.log("enter");
-      }
-    }}
-  />
+  <span class="flex gap-4">
+    <Input
+      class="w-full h-12 text-lg xl:text-xl"
+      placeholder="Ask away..."
+      bind:value={query}
+      bind:disabled={isQuerying}
+    />
+    <Button class="h-12" bind:disabled={isQuerying} on:click={makeQuery}
+      >Submit</Button
+    >
+  </span>
+
+  {#if reply}
+    <div
+      class="dark:bg-background bg-indigo-50 flex py-2 px-8 border-b-[1px] justify-between items-center w-full rounded-2xl mt-8"
+    >
+      {#each reply?.choices as replyChoice}
+        <p>{replyChoice.message.content}</p>
+      {/each}
+    </div>
+  {/if}
 </div>
--- src/lib/pages/Main.svelte
+++ src/lib/pages/Main.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <div class="w-full px-4 h-dvh flex flex-col justify-center items-center">
-  <div class="grid w-full max-w-sm items-center gap-1.5 mb-8">
+  <!-- <div class="grid w-full max-w-sm items-center gap-1.5 mb-8">
     <Label for="model">Model</Label>
     <Input id="model" type="file" />
-  </div>
+  </div> -->
 
   <MainInput />
 </div>
