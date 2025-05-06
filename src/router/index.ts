f638400c579105ea8ecdc73177cd2d17ca5f7130
aalto95 <stanportableuse@gmail.com>
2025-05-06 15:27:57 +0900

    Add nprogress bar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import NotFound from '@/pages/NotFound.vue';
+import { useNProgress } from '@vueuse/integrations/useNProgress';
 import { createRouter, createWebHistory } from 'vue-router';
+
+const nprogress = useNProgress();
 
 const routes = [
   { path: '/', name: 'Main', component: () => import('@/pages/Main.vue') },
@@ -14,6 +17,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach((_to, _from, next) => {
+  nprogress.start();
+  next();
+});
+
+router.afterEach(() => {
+  nprogress.done();
 });
 
 export default router;
